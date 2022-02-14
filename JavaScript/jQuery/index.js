4131fa662dc18c8f27838d09bf0a0c1876087b1b
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2022-02-14 20:38:07 +0530

    Add files via upload

---
+++ JavaScript/jQuery/index.js
@@ -0,0 +1,24 @@
+// $('h1').css('color','red');
+$('h1').addClass("large_head"); //can also add mutiple clasess
+
+// $('button').text("Hey")
+// // $('button').html("<b>Hey<b>")
+
+// // ========Attributes====
+
+// $('a').attr("href")  // get
+// $('a').attr("href","https://www.yahoo.com")  // set
+
+// =======================EventLitsener=========
+
+// $(document).keypress(function(event) {
+
+//     $("h1").text(event.key);
+    
+// })
+
+
+// // can also use 'on'
+// $('h1').on('click',function(){
+//     $('h1').css('color','purple');
+// })
